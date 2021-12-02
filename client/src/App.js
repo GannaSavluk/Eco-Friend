7100a9e6bc5efbc82c914588ea6104b9efd51c58
lelonov23 <stepan.leonov777@gmail.com>
2021-12-02 17:25:19 +0300

    merge the maps

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,7 @@ import Header from "./components/Header/Header";
 import Blog from "./components/Blog/Blog";
 import Leaderboard from "./components/Leaderboard/Leaderboard";
 
-import Map from "./components/Map/MapLeha/MapLeha";
 // import Map from "./components/Map/Map";
-import MapGanna from "./components/Map/MapGanna";
 import MapStepa from "./components/Map/MapStepa";
 
 import Signin from "./components/Auth/Signin";
@@ -34,7 +32,6 @@ function App() {
 
       <Routes>
         {/* <Route path="/" element={<Map />} /> */}
-        {/* <Route path="/" element={<MapGanna />} /> */}
         <Route path="/" element={<MapStepa />} />
 
         <Route path="/blog" element={<Blog />} />
