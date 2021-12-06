9f862b5f624f9e78a20e919c7a36a26916446750
lelonov23 <stepan.leonov777@gmail.com>
2021-12-06 11:30:15 +0300

    server changes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,77 +1,82 @@
-const bcrypt = require('bcryptjs');
-const User = require('../db/models/user');
+const bcrypt = require("bcryptjs");
+const User = require("../db/models/user");
 
 function failAuth(res) {
-    res.json(null) 
+  res.json(null);
 }
 
 function serializeUser(user) {
-    return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        role: user.role,
-    };
+  return {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
 }
 
 exports.createUserAndSession = async (req, res, next) => {
-    const { name, password, email } = req.body;
-    try {
-        const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
+  const { name, password, email } = req.body;
+  try {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-        const user = await User.create({
-            name: name,
-            password: hashedPassword,
-            email: email
-        });
+    const user = await User.create({
+      name: name,
+      password: hashedPassword,
+      email: email,
+    });
 
-        req.session.user = serializeUser(user); 
-
-    } catch (err) {
-        console.error('Err message:', err.message);
-        console.error('Err code', err.code);
-        return failAuth(res);
-    }
-    res.json(req.session.user.id) //TODO
-    res.status(200).end(); // ответ 200 + отправка cookies в заголовке на сервер
+    req.session.user = serializeUser(user);
+  } catch (err) {
+    console.error("Err message:", err.message);
+    console.error("Err code", err.code);
+    return failAuth(res);
+  }
+  res.json(req.session.user.id); //TODO
+  res.status(200).end(); // ответ 200 + отправка cookies в заголовке на сервер
 };
 
 exports.checkUserAndCreateSession = async (req, res, next) => {
-    const { email, password } = req.body;
-    // console.log(email, password)
-    try {
-        // Пытаемся сначала найти пользователя в БД
-        const user = await User.findOne({ email: email });
-        if (!user) return failAuth(res);
+  const { email, password } = req.body;
+  // console.log(email, password)
+  try {
+    // Пытаемся сначала найти пользователя в БД
+    const user = await User.findOne({ email: email });
+    if (!user) return failAuth(res);
 
-        // Сравниваем хэш в БД с хэшем введённого пароля
-        const isValidPassword = await bcrypt.compare(password, user.password);
-        // const isValidPassword = user.password === password;
+    // Сравниваем хэш в БД с хэшем введённого пароля
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    // const isValidPassword = user.password === password;
 
-        if (!isValidPassword) return failAuth(res);
+    if (!isValidPassword) return failAuth(res);
 
-        // записываем в req.session.user данные (id & name) (создаем сессию)
-        req.session.user = serializeUser(user);
-
-    } catch (err) {
-        console.error('Err message:', err.message);
-        console.error('Err code', err.code);
-        return failAuth(res);
-    }
-    // console.log(req.session)
-    res.json(req.session.user.id)
-    res.status(200).end(); // ответ 200 + отправка cookies в заголовке на сервер
+    // записываем в req.session.user данные (id & name) (создаем сессию)
+    req.session.user = serializeUser(user);
+  } catch (err) {
+    console.error("Err message:", err.message);
+    console.error("Err code", err.code);
+    return failAuth(res);
+  }
+  // console.log(req.session)
+  res.json(req.session.user.id);
+  res.status(200).end(); // ответ 200 + отправка cookies в заголовке на сервер
 };
 
 exports.destroySession = (req, res, next) => {
-    try {
-        req.session.destroy((err) => {
-            if (err) return next(err);
-            res.clearCookie('sid');
-            res.send({})
-        });
-    } catch (error) {
-        console.log(error.message);
-    }
+  try {
+    req.session.destroy((err) => {
+      if (err) return next(err);
+      res.clearCookie("sid");
+      res.send({});
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+exports.deleteUser = (req, res) => {
+  try {
+    console.log(req.params.id);
+    // логика удаления юзера
+  } catch (error) {}
 };
--- server/routers/auth.js
+++ server/routers/auth.js
@@ -1,22 +1,27 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 const {
-    signupRender, signinRender, createUserAndSession, checkUserAndCreateSession, destroySession
-} = require('../controllers/authController');
+  signupRender,
+  signinRender,
+  createUserAndSession,
+  checkUserAndCreateSession,
+  destroySession,
+  deleteUser,
+} = require("../controllers/authController");
 // const { isValidPassword } = require('../middleware/authMiddleware');
 
+router.post("/signup", createUserAndSession);
+router.post("/signin", checkUserAndCreateSession);
+router.post("/logout", destroySession);
+router.delete("/:id", deleteUser);
 
-router.post('/signup', createUserAndSession);
-router.post('/signin', checkUserAndCreateSession);
-router.post('/logout', destroySession);
-
-router.get('/check', (req, res) => {
-    // req.session.maxAge = 1000 //TODO использовать, если будем делать "запомнить меня" при регестрации
-    console.log('req.session', req.session)
-    if (req.session.user.role === 1)  res.json(req.session.user)
-    else res.json({})
+router.get("/check", (req, res) => {
+  // req.session.maxAge = 1000 //TODO использовать, если будем делать "запомнить меня" при регестрации
+  console.log("req.session", req.session);
+  if (req.session.user.role === 1) res.json(req.session.user);
+  else res.json({});
 });
 
 module.exports = router;
