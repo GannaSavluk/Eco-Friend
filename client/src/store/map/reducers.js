4b813ddd970ffd1b0a6b3c6a3baaee1aec59af4f
Ганна Савлук <savlukganna@gmail.com>
2021-12-07 18:19:03 +0400

    started Drawer

--- client/src/store/map/reducers.js
+++ client/src/store/map/reducers.js
@@ -2,16 +2,37 @@ import ACTypes from "../types";
 
 const initialState = {
   map: [],
-  point: [],// позиция по клику
+  point: [], // позиция по клику
+  currentImg: null,
+  currentPoint: {}
 };
 
 export const map = (state = initialState, action) => {
+  // console.log("MAP", state.map);
   switch (action.type) {
     case ACTypes.MAP:
       return { ...state, map: action.payload.map };
 
     case ACTypes.POINT:
       return { ...state, point: action.payload.point }; // позиция по клику
+
+    case ACTypes.CURRENT_IMG_MAP:
+      return { ...state, currentImg: action.payload.img };
+
+    case ACTypes.CONFIRM_POINT:
+      console.log('1111')
+      state.map = state.map.map((point) => {
+        console.log('action.payload.point._id', action.payload.point)
+        if (point?._id === action.payload.point._id) {
+          return {
+            ...point,
+            confirmed: true,
+          };
+        }
+        return point;
+      });
+      state.currentPoint = {};
+      return state;
 
     default:
       return state;
--- server/db/models/map.js
+++ server/db/models/map.js
@@ -4,9 +4,10 @@ const MapSchema = new mongoose.Schema({
   category: { type: String, required: true },
   description: { type: String },
   author: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
-  imgs: { type: Array },
+  img: { type: String },
   coordinates: { type: Array, unique: true },
-  date: { type: Date, default: new Date() },
+  date: { type: Date },
+  confirmed: {type: Boolean, default: false },
   adress: { type: String },
 });
 
