9d353d3582b4ee585d2727810930ac6ac507e8aa
Ганна Савлук <savlukganna@gmail.com>
2021-12-14 17:48:33 +0400

    ready to deploiy

--- client/src/components/Auth/Logout.js
+++ client/src/components/Auth/Logout.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from "react-redux";
 import { useNavigate } from 'react-router-dom';
-import { logoutThunk } from '../../store/auth/actions'
+import { logoutThunk } from '../../store/user/actions'
 
 function Logout() {
     const navigate = useNavigate()
